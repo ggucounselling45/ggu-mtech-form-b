2fbe2dc25fb0e6088790688f63879aabe700b22d
pravesh1731 <706890pk@gmail.com>
2026-07-23 10:08:24 +0530

    versal.json is edited

--- src/utils/uploadToCloudinary.js
+++ src/utils/uploadToCloudinary.js
@@ -1,24 +1,21 @@
 import cloudinary from "../config/cloudinary.js";
-import streamifier from "streamifier";
 
-const uploadToCloudinary = (buffer, folder) => {
+const uploadToCloudinary = (fileBuffer, folder) => {
   return new Promise((resolve, reject) => {
-    const stream = cloudinary.uploader.upload_stream(
-      {
-        folder,
-        resource_type: "auto",
-      },
+    const uploadStream = cloudinary.uploader.upload_stream(
+      { folder },
       (error, result) => {
-        if (error) return reject(error);
-
+        if (error) {
+          return reject(error);
+        }
         resolve({
           url: result.secure_url,
           public_id: result.public_id,
         });
-      }
+      },
     );
 
-    streamifier.createReadStream(buffer).pipe(stream);
+    uploadStream.end(fileBuffer); // 👈 pipes buffer directly to Cloudinary
   });
 };
 
